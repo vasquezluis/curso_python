a88a247fa871c2ed244bed517585c78bc02371d4
Luis Antonio Vásquez Tiu <44751046+vasquezluis@users.noreply.github.com>
2023-08-03 21:46:09 -0600

    Update pruebaCrawler2.js

--- elasticSearch/prueba1/pruebaCrawler2.js
+++ elasticSearch/prueba1/pruebaCrawler2.js
@@ -1,11 +1,11 @@
 const { Client } = require("@elastic/elasticsearch");
 
 const client = new Client({
-  node: "https://ee80a4b757ac4cf7aa931f2dfd04bc67.us-central1.gcp.cloud.es.io", // Elasticsearch endpoint
+  node: "", // Elasticsearch endpoint
   auth: {
     apiKey: {
       // API key ID and secret
-      id: "ee80a4b757ac4cf7aa931f2dfd04bc67",
+      id: "",
       api_key: "",
     },
   },
