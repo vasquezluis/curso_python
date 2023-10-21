48ddce48a9fa9ff19c01d95ea1207c2205b0784a
vasquezluis <luivasquez95@gmail.com>
2023-10-21 10:32:33 -0600

    Delete elastic example

--- elasticSearch/djangoLangchain/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('chat/', views.chat),
-  path('chatgptcsv/', views.langchainCSV),
-  path('chatgptsqlite/', views.langchainSqlite),
-  # path('chatgptmysql/', views.langchainmysql)
-]
--- elasticSearch/prueba2/sqlite_langch.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# conect to anew db file called mydatabse.db
-conn = sqlite3.connect('mydatabase.db')
-
-# close the database connection
-conn.close()
